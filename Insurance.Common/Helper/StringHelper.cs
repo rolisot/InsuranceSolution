09b4229ac5f50f8f26a55689c3b5ab537f0b4727
ronaldo <ronaldo@cwi.com.br>
2016-10-14 17:33:15 -0300

    Reformulação da estrutura do projeto

---
+++ Insurance.Common/Helper/StringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Insurance.Common.Helper
+{
+    public class StringHelper
+    {
+        public static string RemoveCharacters(string text, string regex)
+        {
+            Regex reg = new Regex(regex);
+            return reg.Replace(text, string.Empty);
+        }
+
+        public static string CleanCpf(string cpf)
+        {
+            return RemoveCharacters(cpf, @"[^0-9]");
+        }
+    }
+}
